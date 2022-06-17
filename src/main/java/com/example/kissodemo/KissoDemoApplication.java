4ba76f888c754eca1ca578a14f8583fdbec42498
zhaokai <alexander.zk@foxmail.com>
2022-06-17 12:45:17 +0800

    新建SpringBoot项目

---
+++ src/main/java/com/example/kissodemo/KissoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kissodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KissoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KissoDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kissodemo/KissoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kissodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KissoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
